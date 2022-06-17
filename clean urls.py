a834b4f6715c51f2b1c2e527dea56f33dbce84f3
Kevin Le <kevin.k.le.2020@gmail.com>
2022-06-17 08:38:17 -0500

    Added more clarity to purpose of script, also began working on it in a JupyterNotebook as it is much more convenient

--- clean urls.py
+++ clean urls.py
@@ -2,3 +2,4 @@
 # I want to remove all links that aren't degree plans.
 # Note how degree plans are formatted as:
 # "https://www.utdallas.edu/fact-sheets/" + (some department) + "/" + (degree plan name)
+
